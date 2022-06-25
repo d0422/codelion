0dedeb428f09a8059bb173ac12716e63e12be17f
d0422 <rlfehd2013@naver.com>
2022-06-25 12:00:02 +0900

    jasoseo

---
+++ javascript lecture/자소서 글자수 계산기/script.js
@@ -0,0 +1,10 @@
+const counter=()=>{
+let content = document.getElementById("jasoseo").value;
+if (content.length>200){
+    content=content.substring(0,200);
+    document.getElementById('jasoseo').value=content;
+}
+document.getElementById(
+  "count"
+).innerHTML = `(${content.length}/200)`;
+}
